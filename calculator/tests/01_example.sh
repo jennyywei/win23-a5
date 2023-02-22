98c40808296598f90b5f81823afc1ab50576bbe7
Jenny Wei <jenwwwww@gmail.com>
2023-02-21 22:25:59 -0800

    make mytests.sh executable

--- calculator/tests/01_example.sh
+++ calculator/tests/01_example.sh
@@ -29,26 +29,3 @@ if $CALCULATOR 3 @ 2; then  # If the return code of $PROGRAM is zero (i.e. succe
   echo 'ERROR! An invalid run of the application (3 @ 2) apparently succeeded?!'
   exit 1
 fi
-
-if [[ $($CALCULATOR -1 - -4) -ne 3 ]]; then  
-  echo 'ERROR! A valid run of the calculator (-1 - -4) failed to produce 3 as an output!'
-  exit 1
-fi
-
-# Test 02: Ensure addition works with negative inputs
-if [[ $($CALCULATOR -1 + 4) -ne 3 ]]; then  
-  echo 'ERROR! A valid run of the calculator (-1 + 4) failed to produce 3 as an output!'
-  exit 1
-fi
-
-# Test 03: Ensure program works with division
-if [[ $($CALCULATOR 8 / 4) -ne 2 ]]; then  
-  echo 'ERROR! A valid run of the calculator (8 / 4) failed to produce 2 as an output!'
-  exit 1
-fi
-
-# Test 03: Ensure program works with multiplication
-if [[ $($CALCULATOR 8 * 4) -ne 32 ]]; then  
-  echo 'ERROR! A valid run of the calculator (8 * 4) failed to produce 32 as an output!'
-  exit 1
-fi
